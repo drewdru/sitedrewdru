3b19de4165d86425e0171caf2025b5901ce9826c
Drew Dru <drew.drux@gmail.com>
2019-05-13 14:25:51 +0700

    init

---
+++ src/views/Home/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  </div>
+</template>
+
+<script lang="ts" src="./home.ts"></script>
+<style scoped lang="less" src="./home.less"></style>
---
+++ src/views/Home/home.ts
@@ -0,0 +1,9 @@
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from '@/components/HelloWorld/HelloWorld.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+    HelloWorld,
+  },
+})
+export default class Home extends Vue {}
