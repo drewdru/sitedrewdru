8fcf390a2241a96791f9fd859bef3ea5df097fd0
Drew Dru <drew.drux@gmail.com>
2020-02-18 13:53:09 +0700

    cache language test vui-select

--- src/views/index/About/about.ts
+++ src/views/index/About/about.ts
@@ -1,6 +1,8 @@
 import { Component, Emit, Prop, Vue } from 'vue-property-decorator';
 import { SmartQuery } from 'vue-apollo-decorator';
 import gql from 'graphql-tag';
+import VuiSelect from '@/components/VuiSelect/VuiSelect.vue';
+
 
 const TaskQuery = gql`
   query {
@@ -36,16 +38,39 @@ const TaskUpdate = gql`mutation updateTask($id: String, $IsDone: Boolean) {
     ok
   }
 }`;
+interface Languages {
+  locale: string;
+  flag: string;
+  title: string;
+}
 
 
 @Component({
   components: {
+    VuiSelect,
   },
 })
 export default class About extends Vue {
 
   private name: string = '';
   private description: string = '';
+
+  private languages: Languages[] = [
+    {locale: 'en', flag: 'us', title: 'English'},
+    {locale: 'ru', flag: 'ru', title: 'Russian'},
+    {locale: 'ru', flag: 'ru', title: 'Russianssssssssssssssssssssssssdasda sd asd as as ds asd sa d'},
+    {locale: 'ru', flag: 'ru', title: 'Russianassssssda sd as as das dasd asd as das '},
+    {locale: 'ru', flag: 'ru', title: 'Russianasd asd asd as das das das d '},
+    {locale: 'ru', flag: 'ru', title: 'Russianasd asd asd as as '},
+    {locale: 'ru', flag: 'ru', title: 'Russianasd asd asd as da'},
+    {locale: 'ru', flag: 'ru', title: 'Russianasd asd asd '},
+    {locale: 'ru', flag: 'ru', title: 'Russianasd asd asd as das das sd asd asd as'},
+    {locale: 'ru', flag: 'ru', title: 'Russianasd as das das as dasd as asd asdas dsa dasd as dasd asd asd asd asd asd asd asdsa '},
+    {locale: 'ru', flag: 'ru', title: 'Russian'},
+    {locale: 'ru', flag: 'ru', title: 'Russian'},
+    {locale: 'ru', flag: 'ru', title: 'Russian'},
+  ];
+  private selected: Languages|any = {};
 
   @SmartQuery(TaskQuery) private tasks: any;
   // // OR
@@ -56,6 +81,17 @@ export default class About extends Vue {
   //   // }
   // })
   // task: Task;
+
+  private created() {
+    this.selected = this.languages.filter(
+      (c) => c.locale === this.$i18n.locale,
+    )[0];
+  }
+  @Emit()
+  private localeChange() {
+    this.$i18n.locale = this.selected.locale;
+    localStorage.setItem('language', this.selected.locale);
+  }
 
   @Emit()
   private async createTask() {
