61d655e44ff0e54b3c09dcdf6126565210afc25a
Drew Dru <drew.drux@gmail.com>
2021-01-09 05:50:41 +0300

    refactoring

--- src/components/SideBar/side-bar.ts
+++ src/components/SideBar/side-bar.ts
@@ -4,16 +4,16 @@ import { Component, Prop, Emit, Vue, Model } from 'vue-property-decorator';
   },
 })
 export default class SideBar extends Vue {
-  @Model('change', { type: Boolean }) readonly isOpen!: boolean
+  @Model('change', { type: Boolean }) public readonly isOpen!: boolean;
 
   get isSideBar(): boolean {
     return this.isOpen;
   }
 
   set isSideBar(value: boolean) {
-    this.$emit('change', value)
+    this.$emit('change', value);
   }
-  
+
   // private computed() {
   //   checkedValue: {
   //     get() {
